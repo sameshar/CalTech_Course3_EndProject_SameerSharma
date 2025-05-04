89565ecaa183adc9bcd61a232c2587a43550bf7a
sameshar <sameerdev1@outlook.com>
2025-05-04 12:33:50 -0400

    First Commit, Added the following functionality *Create customer account*.

---
+++ Project_MyCabBuddy/Code/src/test/java/com/BookingMyCabBuddy/BookingMyCabBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BookingMyCabBuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingMyCabBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
